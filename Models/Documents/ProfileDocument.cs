9764391df8cd0d47d4c07fb9e1f67fdf0f1e11ed
Ati <bricegomis@users.noreply.github.com>
2025-10-15 00:57:05 +0200

    Change DisplayName to Name

--- Models/Documents/ProfileDocument.cs
+++ Models/Documents/ProfileDocument.cs
@@ -7,7 +7,7 @@ public class ProfileDocument : IDateTracked
     public required string Id { get; set; }
     public required string Email { get; set; }
     public string? AvatarUrl { get; set; }
-    public string? DisplayName { get; set; }
+    public string? Name { get; set; }
     public DateTimeOffset DateLastConnection { get; set; }
     public DateTimeOffset DateCreated { get; set; }
     public DateTimeOffset DateModified { get; set; }
