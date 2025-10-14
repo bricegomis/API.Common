e331b03d54397b986d8cb97b1593d0f377e94ff3
Ati <bricegomis@users.noreply.github.com>
2025-10-15 01:10:13 +0200

    dto

--- Models/Dto/ProfileDto.cs
+++ Models/Dto/ProfileDto.cs
@@ -4,7 +4,7 @@ public class ProfileDto
 {
     public required string Id { get; set; }
     public required string Email { get; set; }
-    public string? DisplayName { get; set; }
+    public string? Name { get; set; }
     public string? AvatarUrl { get; set; }
     public DateTimeOffset DateLastConnection { get; set; }
     public DateTimeOffset DateCreated { get; set; }
